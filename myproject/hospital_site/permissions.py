ad8a12735e1526e36ad26af096502460b3b2d5dd
fatima0314 <fattum_03@icloud.com>
2025-01-20 19:49:28 +0600

    Исправила ошибки

--- myproject/hospital_site/permissions.py
+++ myproject/hospital_site/permissions.py
@@ -1,5 +1,14 @@
 from rest_framework import permissions
 
+class CheckAdmin(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return request.user.role == 'admin'
+
+
+class CheckAll(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.role == 'admin'
 
 
 class CheckAppointment(permissions.BasePermission):
--- myproject/hospital_site/views.py
+++ myproject/hospital_site/views.py
@@ -1,9 +1,10 @@
 from rest_framework import viewsets, generics, permissions, status
+from rest_framework.exceptions import ValidationError
 from .serializers import *
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 from .paginations import DoctorPagination
-from .permissions import CheckAppointment, CheckMedicalRecord
+from .permissions import CheckAppointment, CheckMedicalRecord, CheckAdmin, CheckAll
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
 
@@ -13,11 +14,15 @@ class RegisterView(generics.CreateAPIView):
     serializer_class = ProfileRegisterSerializer
 
     def create(self, request, *args, **kwargs):
-        serializer =self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.save()
-        return Response(serializer.data,  status=status.HTTP_201_CREATED)
-
+       try:
+           serializer = self.get_serializer(data=request.data)
+           serializer.is_valid(raise_exception=True)
+           user = serializer.save()
+           return Response(serializer.data, status=status.HTTP_201_CREATED)
+       except serializers.ValidationError:
+           return Response({'detail':'Туура эмес маалымат'}, status.HTTP_400_BAD_REQUEST)
+       except NameError as e:
+           return Response({'detail':f'{e}, Код туура эмес'}, status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 class CustomLoginView(TokenObtainPairView):
@@ -34,17 +39,16 @@ class CustomLoginView(TokenObtainPairView):
         return Response(serializer.data, status = status.HTTP_200_OK)
 
 
-
 class LogoutView(generics.GenericAPIView):
+
     def post(self, request, *args, **kwargs ):
         try:
             refresh_token = request.data["refresh"]
             token = RefreshToken(refresh_token)
             token.blacklist()
             return Response(status=status.HTTP_205_RESET_CONTENT)
-        except Exception:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-
+        except KeyError:
+            return Response({'detail':'Ключ туура эмес'}, status.HTTP_400_BAD_REQUEST)
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
@@ -55,18 +59,15 @@ class ProfileViewSet(viewsets.ModelViewSet):
         return Profile.objects.filter(id=self.request.user.id)
 
 
-
 class ProfileCreateApiView(generics.CreateAPIView):
     serializer_class = ProfileSerializer
-    permission_classes = [permissions.IsAdminUser]
-
+    permission_classes = [CheckAll]
 
 
 class SpecialityApiView(generics.ListCreateAPIView):
     queryset = Speciality.objects.all()
     serializer_class = SpecialitySerializer
-    permission_classes = [permissions.IsAdminUser]
-
+    permission_classes = [CheckAdmin]
 
 
 class DoctorListApiView(generics.ListAPIView):
@@ -75,8 +76,7 @@ class DoctorListApiView(generics.ListAPIView):
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     search_fields = ['specialty', 'price', 'working_days']
     ordering_fields = ['price']
-    pagination_class = [DoctorPagination]
-
+    pagination_class = DoctorPagination
 
 
 class DoctorDetailApiView(generics.RetrieveAPIView):
@@ -84,11 +84,9 @@ class DoctorDetailApiView(generics.RetrieveAPIView):
     serializer_class = DoctorDetailSerializer
 
 
-
 class DoctorCreateApiView(generics.CreateAPIView):
     serializer_class = DoctorCreateSerializer
-    permission_classes = [permissions.IsAdminUser]
-
+    permission_classes = [CheckAll]
 
 
 class PatientListApiView(generics.ListAPIView):
@@ -96,11 +94,9 @@ class PatientListApiView(generics.ListAPIView):
     serializer_class = PatientListSerializer
 
 
-
 class PatientDetailApiView(generics.RetrieveAPIView):
     queryset = Patient.objects.all()
     serializer_class = PatientDetailSerializer
-
 
 
 class DepartmentViewSet(viewsets.ModelViewSet):
@@ -109,49 +105,42 @@ class DepartmentViewSet(viewsets.ModelViewSet):
     filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_fields = ['name']
     searching_fields = ['name']
-    permission_classes = [permissions.IsAdminUser]
-
+    permission_classes = [CheckAdmin]
 
 
 class AppointmentViewSet(viewsets.ModelViewSet):
     queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
-    permission_classes = [permissions.IsAdminUser]
-
+    permission_classes = [CheckAdmin]
 
 
 class AppointmentCreateApiView(generics.CreateAPIView):
     serializer_class = AppointmentSerializer
-    permission_classes =  [permissions.IsAdminUser, CheckAppointment]
-
+    permission_classes =  [CheckAll, CheckAppointment]
 
 
 class MedicalRecordApiView(generics.ListCreateAPIView):
     queryset = MedicalRecord.objects.all()
     serializer_class = MedicalRecordSerializer
-    permission_classes = [permissions.IsAdminUser, CheckMedicalRecord]
-
+    permission_classes = [CheckAdmin, CheckMedicalRecord]
 
 
 class PrescriptionsViewSet(viewsets.ModelViewSet):
     queryset = Prescriptions.objects.all()
     serializer_class = PrescriptionsSerializer
-    permission_classes = [permissions.IsAdminUser]
-
+    permission_classes = [CheckAdmin]
 
 
 class BillingViewSet(viewsets.ModelViewSet):
     queryset = Billing.objects.all()
     serializer_class = BillingSerializer
-    permission_classes = [permissions.IsAdminUser]
-
+    permission_classes = [CheckAdmin]
 
 
 class WardViewSet(viewsets.ModelViewSet):
     queryset = Ward.objects.all()
     serializer_class = WardSerializer
-    permission_classes = [permissions.IsAdminUser]
-
+    permission_classes = [CheckAdmin]
 
 
 class FeedbackViewSet(viewsets.ModelViewSet):
