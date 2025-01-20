0bfe47cd3ce565b51885ae987ef5a3346f4b083d
fatima0314 <fattum_03@icloud.com>
2025-01-20 19:52:13 +0600

    aaa

--- myproject/hospital_site/permissions.py
+++ myproject/hospital_site/permissions.py
@@ -1,5 +1,6 @@
 from rest_framework import permissions
 
+
 class CheckAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
@@ -16,7 +17,6 @@ class CheckAppointment(permissions.BasePermission):
         if request.method  in permissions.SAFE_METHODS:
             return True
         return request.user.role == 'patient'
-
 
 
 class CheckMedicalRecord(permissions.BasePermission):
